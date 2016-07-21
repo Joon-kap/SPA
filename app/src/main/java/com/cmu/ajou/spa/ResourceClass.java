9beb7c85690842c76e9d5d59610eebc51ce37c85
Minsung <kimmsql@naver.com>
2016-07-20 20:15:46 -0400

    [07.20] for review ver.0.1 - 액션 바 색상 변경까지 완료

--- app/src/main/java/com/cmu/ajou/spa/ResourceClass.java
+++ app/src/main/java/com/cmu/ajou/spa/ResourceClass.java
@@ -5,6 +5,6 @@ package com.cmu.ajou.spa;
  */
 public class ResourceClass {
 
-    public static String server_ip = "http://128.237.196.213:8080";
-//    public static String server_ip = "http://192.168.1.6:8080";
+    public static String server_ip = "http://128.237.211.41:8080";
+//  public static String server_ip = "http://192.168.1.6:8080";
 }
